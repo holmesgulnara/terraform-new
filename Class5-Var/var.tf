56f7c43c158b41df7343b99bbb9e0977d617976d
holmesgulnara <holmesgulnara@gmail.com>
2025-01-29 15:05:24 +0000

    new codessss

---
+++ Class5-Var/var.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  description = "Provide aws region"
+}
+
+variable "vpc_cidr" {
+  type = string
+  description = "Provide cidr block"
+}
+
+variable "subnet1_cidr" {
+  type = string
+  description = "Provide subnet1 cidr block"
+}
