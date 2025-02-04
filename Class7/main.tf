f1108c3749957ebe0b4c6241773d55d36b8194c6
holmesgulnara <holmesgulnara@gmail.com>
2025-02-04 03:44:57 +0000

    provisioners

--- Class7/main.tf
+++ Class7/main.tf
@@ -1,8 +1,37 @@
 provider "aws" {
-    region = "us-east'2"  
+  region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-018875e7376831abe"
-  instance_type = "t2.micro"
+  ami                    = "ami-018875e7376831abe"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = aws_key_pair.deployer.key_name
+
+  # connection {
+  #   type     = "ssh"
+  #   user     = "ec2-user"
+  #   private_key = file("~/.ssh/id_rsa")
+  #   host     = self.public_ip
+  # }
+
+  # provisioner "file" {
+  # source = "./httpd.sh"
+  # destination = "./httpd.sh"
+  # }
+
+  # provisioner "remote-exec" {
+  #   script = "./httpd.sh"
+  # }
+  # }
+
+  # resource "null_resource" "hello" {
+  #   provisioner "local-exec" {
+  #     command = "mkdir kaizen && touch hello"
+  #   }
 }
